e5314b3d3042088a5f2bd28a73c3c48b8da60e16
Brendan McShane <brendanmcs7@gmail.com>
2019-05-29 19:34:00 -0400

    remove root

--- Fraction/Default.aspx.cs
+++ Fraction/Default.aspx.cs
@@ -48,13 +48,17 @@ namespace Fraction
 
                 output.Simp();
 
-                if (output.Numerator == 0)
+                if (output.Numerator == 0 && output.WholeNumber != 0)
                 {
-                    Output.Text = output.WholeNumber+"";
+                    Output.Text = output.WholeNumber + "";
+                }
+                else if (output.WholeNumber != 0 && output.Numerator != 0)
+                {
+                    Output.Text = output.WholeNumber + " " + output.Numerator + "/" + output.Denominator;
                 }
                 else
                 {
-                    Output.Text = output.WholeNumber + " " + output.Numerator + "/" + output.Denominator;
+                    Output.Text = output.Numerator + "/" + output.Denominator;
                 }
 
             }
@@ -96,12 +100,17 @@ namespace Fraction
                 output.Numerator = (frac1.Numerator) * (output.Denominator / frac1.Denominator) - (frac2.Numerator) * (output.Denominator / frac2.Denominator);
 
                 output.Simp();
-                if (output.Numerator == 0)
+                if (output.Numerator == 0 && output.WholeNumber != 0)
                 {
                     Output.Text = output.WholeNumber + "";
-                } else
+                }
+                else if (output.WholeNumber != 0 && output.Numerator != 0)
                 {
                     Output.Text = output.WholeNumber + " " + output.Numerator + "/" + output.Denominator;
+                }
+                else
+                {
+                    Output.Text = output.Numerator + "/" + output.Denominator;
                 }
 
             }
@@ -143,13 +152,17 @@ namespace Fraction
                 };
 
                 output.Simp();
-                if (output.Numerator == 0)
+                if (output.Numerator == 0 && output.WholeNumber != 0)
                 {
                     Output.Text = output.WholeNumber + "";
                 }
-                else
+                else if (output.WholeNumber != 0 && output.Numerator != 0)
                 {
                     Output.Text = output.WholeNumber + " " + output.Numerator + "/" + output.Denominator;
+                }
+                else
+                {
+                    Output.Text = output.Numerator + "/" + output.Denominator;
                 }
             }
         }
@@ -191,59 +204,17 @@ namespace Fraction
 
                 output.Simp();
 
-                if (output.Numerator == 0)
+                if (output.Numerator == 0 && output.WholeNumber != 0)
                 {
                     Output.Text = output.WholeNumber + "";
                 }
-                else
-                {
-                    Output.Text =output.WholeNumber+" "+output.Numerator + "/" + output.Denominator;
-                }
-            }
-        }
-
-        protected void Root_Click(object sender, EventArgs e)
-        {
-            if (Page.IsValid)
-            {
-                Fractions frac1 = new Fractions();
-                Fractions frac2 = new Fractions();
-
-                try
-                {
-                    frac1.FractionConvert(Fraction1.Text);
-                }
-                catch
-                {
-                    error1.Visible = true;
-                    error1.Text = "Not a valid fraction";
-                }
-
-                try
-                {
-                    frac2.FractionConvert(Fraction2.Text);
-                }
-                catch
-                {
-                    error2.Visible = true;
-                    error2.Text = "Not a valid fraction";
-                }
-                error1.Visible = false;
-                error2.Visible = false;
-
-                Fractions output = new Fractions();
-
-                
-
-                output.Simp();
-
-                if (output.Numerator == 0)
-                {
-                    Output.Text = output.WholeNumber + "";
-                }
-                else
+                else if (output.WholeNumber != 0 && output.Numerator != 0)
                 {
                     Output.Text = output.WholeNumber + " " + output.Numerator + "/" + output.Denominator;
+                }
+                else
+                {
+                    Output.Text = output.Numerator + "/" + output.Denominator;
                 }
             }
         }
